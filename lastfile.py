916db328b181ae0862025d7fcc1c624d95deb4b8
hex <admin@MacBook-Air-Admin.local>
2023-02-09 16:27:56 +0300

    modified:   lastfile.py

--- lastfile.py
+++ lastfile.py
@@ -1,32 +1,24 @@
-import OPi.GPIO as GPIO
 import time
-import os
-import shutil
 import requests
 
 URL = 'http://127.0.0.1'
 
-def read_file(last_file):
-    try:
-        file = ''
-        with open('/home/klipper/printer_button/lastfile.txt', 'r') as out:
-            file = out.read()
-            out.close()
-            return last_file == file
-    except:
-        read_file(last_file)
-
-try:
-    last_file = requests.get(URL + 
-'/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
-
-    while read_file(last_file) or last_file == '':
-        last_file = requests.get(URL + 
-'/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
-
-    with open('/home/klipper/printer_button/lastfile.txt', 'w') as out:
-        out.write(last_file)
+def read_file():
+    file = ''
+    with open('/home/klipper/printer_button/lastfile.txt', 'r') as out:
+        file = out.read()
         out.close()
+        return file
 
-    time.sleep(20)
-except: ...
+while True:
+    try:
+        moonraker_file = requests.get(URL + '/printer/objects/query?print_stats').json()['result']['status']['print_stats']['filename']
+        lasfile_txt = read_file()
+        if not moonraker_file == '':
+            if not moonraker_file == lasfile_txt:
+                with open('/home/klipper/printer_button/lastfile.txt', 'w') as out:
+                    out.write(moonraker_file)
+                    out.close()
+        time.sleep(5)
+    except:
+        time.sleep(5)
